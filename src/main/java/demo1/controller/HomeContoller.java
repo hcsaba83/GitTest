95aa031c40144334c4e9f09b091d50a512cd666b
Horváth Csaba <hcsaba83@hotmail.com>
2018-10-21 20:10:25 +0200

    Here we are!

---
+++ src/main/java/demo1/controller/HomeContoller.java
@@ -0,0 +1,19 @@
+package demo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeContoller {
+	
+	@RequestMapping("/")
+	public String stories() {
+		return "stories";
+	}
+	
+	@RequestMapping("/valami")
+	public String valami() {
+		return "valami";
+	}
+
+}
